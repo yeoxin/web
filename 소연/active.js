20770de9f3dd20330ffca25ae9bf6119a20ca789
yeoxin <162659562+yeoxin@users.noreply.github.com>
2024-07-22 23:31:51 +0900

    Add files via upload

---
+++ 소연/active.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    for (let i = 0; i < 1; i++) {
+        alert("바이러스가 감지되었습니다");
+    }
+});
+
+function box() {
+    document.getElementById("box").style.display = "block";
+}
+
+function ew() {
+    document.getElementById("box2").style.display = "block";
+}
